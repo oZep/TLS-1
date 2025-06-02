e50784d7102eec838aa0d1f6817653ddffe739cb
Joey Issa <joeyissa59@gmail.com>
2025-06-02 14:27:54 -0400

    Shooter Movement - DONE

---
+++ TLS/Assets/SpinAroundPivot.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class SpinAroundPivot : MonoBehaviour
+{
+
+	public float rotationSpeed = 100f;
+	public Transform pivotPoint;
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        float rotationAmount = horizontalInput * rotationSpeed * Time.deltaTime;
+
+        if (pivotPoint != null)
+        {
+
+		    transform.RotateAround(pivotPoint.position, Vector3.up, rotationAmount);
+
+	    }    
+    }
+
+    void OnDrawGizmosSelected()
+    {
+	    if (pivotPoint != null)
+	    {
+		    Gizmos.color = Color.red;
+		    Gizmos.DrawSphere(pivotPoint.position, 0.1f); // Draw a small sphere at the pivot
+		    Gizmos.DrawLine(transform.position, pivotPoint.position); // Draw a line to the pivot
+	    }
+    }
+}
